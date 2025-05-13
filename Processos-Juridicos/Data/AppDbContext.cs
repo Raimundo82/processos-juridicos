b863f34598e640b1712b5356e15e9931c80c8a23
filipeterrinha <filipeterrinha@gmail.com>
2025-05-13 16:35:32 +0100

    create entities

---
+++ Processos-Juridicos/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Processos_Juridicos.Entities;
+
+namespace Processos_Juridicos.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<Processos_DJContext> options) : base(options)
+    {
+    }
+
+    public virtual DbSet<AccidentTypes> AccidentTypes { get; set; }
+
+    public virtual DbSet<Files> Files { get; set; }
+
+    public virtual DbSet<HarmedOrCasualties> HarmedOrCasualties { get; set; }
+
+    public virtual DbSet<Infringements> Infringements { get; set; }
+
+    public virtual DbSet<ProcessTypes> ProcessTypes { get; set; }
+
+    public virtual DbSet<Processes> Processes { get; set; }
+
+    public virtual DbSet<Sentences> Sentences { get; set; }
+
+    public virtual DbSet<States> States { get; set; }
+
+    public virtual DbSet<Units> Units { get; set; }
+}
