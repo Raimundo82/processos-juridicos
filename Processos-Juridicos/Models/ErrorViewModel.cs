add9b42d43b50dede2f7ab003ac408c19430d1df
M11003725 <M11003725@DAGI-LT-952589>
2025-05-13 10:44:53 +0100

    Adicionar arquivos de projeto.

---
+++ Processos-Juridicos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Processos_Juridicos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
