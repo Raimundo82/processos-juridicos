bd0dd4c41b997cae019b1a58e445be81b5efe3dd
filipeterrinha <filipeterrinha@gmail.com>
2025-05-15 10:21:12 +0100

    Add NToasterNotify

--- Processos-Juridicos/Controllers/HomeController.cs
+++ Processos-Juridicos/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Processos_Juridicos.Data;
 using Processos_Juridicos.Entities;
 using Processos_Juridicos.Models;
+using Processos_Juridicos.Services.Interfaces;
 
 namespace Processos_Juridicos.Controllers
 {
@@ -12,16 +13,19 @@ namespace Processos_Juridicos.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly AppDbContext _context;
+        private readonly IToastNotify _toastNotify;
 
-        public HomeController(ILogger<HomeController> logger, AppDbContext context)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context, IToastNotify toastNotify)
         {
             _logger = logger;
             _context = context;
+            _toastNotify = toastNotify;
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index(int id)
+        public async Task<IActionResult> Index()
         {
+          
            
             return View();
         }
---
+++ Processos-Juridicos/Services/Interfaces/IToastNotify.cs
@@ -0,0 +1,10 @@
+﻿namespace Processos_Juridicos.Services.Interfaces;
+
+public interface IToastNotify
+{
+    Task Sucesso(string msg);
+    Task Error(string msg);
+    Task Info(string msg);
+    Task Alert(string msg);
+    Task Warning(string msg);
+}
