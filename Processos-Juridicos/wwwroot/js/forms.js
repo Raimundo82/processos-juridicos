6731166d1c27d39467dd04add5552b309613e22a
m11003725 <alves.devesa@marinha.pt>
2025-06-26 10:26:53 +0000

    chore(prettier): setup prettier (#62)
    Co-authored-by: João Raimundo <pacheco.raimundo@marinha.pt>
    Reviewed-on: https://devops-01.marinha.pt/marinha-si/Processos-Juridicos/pulls/62
    Reviewed-by: João Raimundo <pacheco.raimundo@marinha.pt>
    Reviewed-by: ESP SIST TECN INF Matos Oliveira <matos.oliveira@marinha.pt>
    Co-authored-by: m11003725 <alves.devesa@marinha.pt>
    Co-committed-by: m11003725 <alves.devesa@marinha.pt>

--- Processos-Juridicos/wwwroot/js/forms.js
+++ Processos-Juridicos/wwwroot/js/forms.js
@@ -1,15 +1,15 @@
 function markFileForDeletion(fileId) {
-    // Remove the table row so it's no longer visible
-    let row = document.getElementById("file-row-" + fileId);
-    if (row) {
-        row.parentNode.removeChild(row);
-    }
+  // Remove the table row so it's no longer visible
+  let row = document.getElementById('file-row-' + fileId);
+  if (row) {
+    row.parentNode.removeChild(row);
+  }
 
-    // Create a new hidden input to mark this file for deletion
-    let container = document.getElementById("deletedFilesContainer");
-    let input = document.createElement("input");
-    input.type = "hidden";
-    input.name = "FilesToRemove"; // This should match the DTO property name
-    input.value = fileId;
-    container.appendChild(input);
+  // Create a new hidden input to mark this file for deletion
+  let container = document.getElementById('deletedFilesContainer');
+  let input = document.createElement('input');
+  input.type = 'hidden';
+  input.name = 'FilesToRemove'; // This should match the DTO property name
+  input.value = fileId;
+  container.appendChild(input);
 }
