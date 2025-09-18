e216f4186e68f4dc36de3b3a33841044f42f4c9e
m11003725 <alves.devesa@marinha.pt>
2025-09-18 10:02:04 +0000

    style: login page and layout (#186)
    Reviewed-on: https://devops-01.marinha.pt/marinha-si/Processos-Juridicos/pulls/186
    Reviewed-by: ESP SIST TECN INF Matos Oliveira <matos.oliveira@marinha.pt>
    Co-authored-by: m11003725 <alves.devesa@marinha.pt>
    Co-committed-by: m11003725 <alves.devesa@marinha.pt>

--- Processos-Juridicos/ViewModels/LoginViewModel.cs
+++ Processos-Juridicos/ViewModels/LoginViewModel.cs
@@ -5,11 +5,11 @@ namespace Processos_Juridicos.ViewModels;
 
 public class LoginViewModel
 {
-    [Required(ErrorMessage = "O nome de utilizador (NII) é obrigatório")]
+    [Required(ErrorMessage = "O campo de NII é obrigatório.")]
     [DisplayName("Utilizador")]
     public required string Username { get; set; }
 
-    [Required(ErrorMessage = "A palavra-passe é obrigatória")]
+    [Required(ErrorMessage = "A campo de palavra-passe é obrigatório.")]
     [DataType(DataType.Password)]
     [DisplayName("Palavra-passe")]
     public required string Password { get; set; }
---
+++ Processos-Juridicos/wwwroot/js/dark-mode.js
@@ -0,0 +1,9 @@
+const toggle = document.getElementById('nightModeToggle');
+
+toggle.addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+
+    const theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+
+    document.cookie = "theme=" + theme + "; path=/; max-age=" + 60 * 60 * 24 * 30;
+});
